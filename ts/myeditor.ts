060acc6abe5b7c3c49af02b69adec426cc06a486
edp1096 <edp1096@naver.com>
2022-06-03 12:43:31 +0900

    시작

--- ts/myeditor.ts
+++ ts/myeditor.ts
@@ -1,13 +1,28 @@
+import { EditorState } from "prosemirror-state"
+import { EditorView } from "prosemirror-view"
 import { Schema, DOMParser } from "prosemirror-model"
 import { schema } from "prosemirror-schema-basic"
 import { addListNodes } from "prosemirror-schema-list"
+import { exampleSetup } from "prosemirror-example-setup"
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
-const mySchema: Schema = new Schema({
+const mySchema = new Schema({
     nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
     marks: schema.spec.marks
 });
+
+/* (window as any).view = new EditorView(document.querySelector("#editor") as HTMLElement, {
+    state: EditorState.create({
+        doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content") as HTMLElement),
+        plugins: exampleSetup({ schema: mySchema })
+    })
+}) */
+
+let state = EditorState.create({
+    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")!),
+    plugins: exampleSetup({ schema: mySchema })
+});
+
+(window as any).view = new EditorView(document.querySelector(".full"), { state })
 
 class MyEditor { }
 
