29db5763b23dac7d288152079b6729503e4782c7
edp1096 <edp1096@naver.com>
2023-10-09 01:23:15 +0900

    menu - working

--- src/helper/upload.ts
+++ src/helper/upload.ts
@@ -98,7 +98,7 @@ async function uploadHandler() {
 }
 
 function getImageUploadMenus(): MenuElement[] {
-    const menuItemUploadImage = { title: "Upload images", icon: setIconElement("bi-images"), run: callBrowseFile }
+    const menuItemUploadImage = { title: "Upload image", icon: setIconElement("bi-cloud-upload"), run: callBrowseFile }
     const uploadMenu = [new MenuItem(menuItemUploadImage)]
 
     return uploadMenu
