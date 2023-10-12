6f94c073925d10b88edf9ae79d311fec845bc6b8
edp1096 <edp1096@naver.com>
2023-10-12 20:37:37 +0900

    misc

--- src/helper/textstyle.ts
+++ src/helper/textstyle.ts
@@ -1,6 +1,5 @@
 import OrderedMap from 'orderedmap'
-import { Schema, Node, NodeSpec, NodeType, Mark, MarkSpec, MarkType, DOMOutputSpec, Fragment, ParseRule } from "prosemirror-model"
-import { NodeSelection, EditorState, TextSelection, SelectionRange, Command, Transaction } from "prosemirror-state"
+import { Mark, MarkSpec, MarkType, DOMOutputSpec, Fragment, ParseRule } from "prosemirror-model"
 
 
 const FontSizeList = [8, 9, 10, 11, 12, 14, 18, 24, 30, 36, 48, 60, 72]
